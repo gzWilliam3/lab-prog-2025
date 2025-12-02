04c361aa6f561878a260866717d050a0a2ff7724
William Gonzalez <gz.william3281@gmail.com>
2025-12-02 10:55:53 -0300

    Modificacion

--- ProyectoWeb/server.js
+++ ProyectoWeb/server.js
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
-//Codigo hecho con patron RPC.
-=======
-//variables globales.
->>>>>>> origin/actualizacionEntreRamas
+//Variables globales
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3000; 
 
 // Configuracion de las rutas de los archivos.
 const dataPath = path.join(__dirname, 'src', 'data', 'datosProductos.json');
 const votosPath = path.join(__dirname, 'src', 'data', 'votos.json');
 const comentariosPath = path.join(__dirname, 'src', 'data', 'comentarios.json');
 
-<<<<<<< HEAD
-// Configuracion de rutas de los archivos de datos.
-const dataPath = path.join(__dirname, 'src', 'data', 'datosProductos.json');
-const votosPath = path.join(__dirname, 'src', 'data', 'votos.json');
-
-let datosComida = {};
-let datosVotos = {}; 
-
-// Permite al servidor leer el cuerpo de las peticiones POST en formato JSON.
-app.use(express.json()); 
-app.use(express.static(path.join(__dirname, 'src')));
-app.use('/assets',express.static(path.join(__dirname, 'src', 'assets')));
-
-function cargarDatos(req, res, next) {
-    // Carga los datos del menú y votos.
-=======
 let datosComida = {};
 let datosVotos = {};
 let datosComentarios = {};
@@ -38,7 +18,6 @@ app.use(express.json()); // Necesario para leer el cuerpo de la petición POST.
 app.use(express.static(path.join(__dirname, 'src'))); // Sirve para mapear los archivos estáticos.
 
 function cargarDatos(req, res, next) {
->>>>>>> origin/actualizacionEntreRamas
     if (Object.keys(datosComida).length === 0) {
         try {
             datosComida = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
@@ -48,18 +27,6 @@ function cargarDatos(req, res, next) {
         }
     }
     if (Object.keys(datosVotos).length === 0) {
-<<<<<<< HEAD
-         try {
-            datosVotos = JSON.parse(fs.readFileSync(votosPath, 'utf8'));
-        } catch (e) {
-            datosVotos = {}; 
-        }
-    }
-    next();
-}
-
-// Rutas HTML
-=======
         try {
             datosVotos = JSON.parse(fs.readFileSync(votosPath, 'utf8'));
         } catch (e) {
@@ -80,7 +47,6 @@ function cargarDatos(req, res, next) {
 app.use('/api', cargarDatos);
 
 // Rutas HTML.
->>>>>>> origin/actualizacionEntreRamas
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'html', 'index.html'));
 });
@@ -89,32 +55,20 @@ app.get('/productos', (req,res) => {
     res.sendFile(path.join(__dirname, 'src', 'html', 'productos.html'));
 });
 
-<<<<<<< HEAD
 app.get('/reviews', (req,res) => {
     res.sendFile(path.join(__dirname, 'src', 'html', 'reviews.html'));
 });
 
-=======
->>>>>>> origin/actualizacionEntreRamas
 app.use('/api', cargarDatos);
 
 // ENDPOINTS GET
 
-<<<<<<< HEAD
-// OBTENER todos los datos del menú.
-app.get('/api/getDatosProductos', (req, res) => {
-    res.json(datosComida); 
-});
-
-// OBTENER productos por categoría.
-=======
 // OBtiene TODOS los datos del menú.
 app.get('/api/datosProductos', (req, res) => {
     res.json(datosComida);
 });
 
 // Obtiene los productos por categoría.
->>>>>>> origin/actualizacionEntreRamas
 app.get('/api/productos/:categoria', (req, res) => {
     const categoriaKey = req.params.categoria;
     const keyConPrefijo = `productos${categoriaKey.charAt(0).toUpperCase() + categoriaKey.slice(1)}`;
@@ -126,16 +80,6 @@ app.get('/api/productos/:categoria', (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-// OBTENER el estado de los votos.
-app.get('/api/getVotos', (req, res) => {
-    res.json(datosVotos);
-});
-
-
-// RECIBIR y guardar un voto.
-app.post('/api/votarPlato', (req, res) => {
-=======
 // Obtiene el estado de los votos.
 app.get('/api/votos', (req, res) => {
     res.json(datosVotos);
@@ -156,18 +100,13 @@ app.get('/api/comentarios/:id', (req, res) => {
 
 // Recibe y guardar un voto.
 app.post('/api/votos', (req, res) => {
->>>>>>> origin/actualizacionEntreRamas
     const { platoId } = req.body;
     
     if (!platoId) {
         return res.status(400).json({ mensaje: 'ID del plato requerido.' });
     }
 
-<<<<<<< HEAD
-    const idStr = String(platoId); 
-=======
     const idStr = String(platoId);
->>>>>>> origin/actualizacionEntreRamas
     datosVotos[idStr] = (datosVotos[idStr] || 0) + 1;
 
     try {
@@ -175,9 +114,6 @@ app.post('/api/votos', (req, res) => {
         res.status(200).json({ mensaje: `Voto registrado para el plato ID ${platoId}. Total de votos: ${datosVotos[idStr]}` });
     } catch (error) {
         console.error("Error al guardar votos.json:", error);
-<<<<<<< HEAD
-        res.status(500).json({ mensaje: 'Error interno del servidor al guardar el voto.' }); //prueba.
-=======
         res.status(500).json({ mensaje: 'Error interno del servidor al guardar el voto.' });
     }
 });
@@ -208,18 +144,11 @@ app.post('/api/comentarios/:id', (req, res) => {
     } catch (error) {
         console.error("Error al guardar comentarios:", error);
         res.status(500).json({ mensaje: "Error interno al guardar comentario." });
->>>>>>> origin/actualizacionEntreRamas
     }
 });
 
 
-<<<<<<< HEAD
-// INICIAR EL SERVIDOR.
-app.listen(PORT, () => {
-    console.log(`Servidor 'Buen Morfar' (RPC) ejecutándose en http://localhost:${PORT}`);
-=======
 //INICIAR EL SERVIDOR
 app.listen(PORT, () => {
     console.log(`Servidor 'Buen Morfar' (RESTful) ejecutándose en http://localhost:${PORT}`);
->>>>>>> origin/actualizacionEntreRamas
 });
